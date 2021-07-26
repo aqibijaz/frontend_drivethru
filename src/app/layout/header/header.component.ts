39d87aced2939335027a35a2f2373a233a958d40
M Tayyab Younas <tayyabjagna@gmail.com>
2021-07-26 23:56:56 +0500

    signou

--- src/app/layout/header/header.component.ts
+++ src/app/layout/header/header.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { CookieService } from 'ngx-cookie-service';
 
 @Component({
   selector: 'app-header',
@@ -7,9 +9,34 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private cookieService: CookieService,
+    private router: Router,
+  ) { }
 
   ngOnInit(): void {
+  }
+
+
+  checkIsUserLogedIn(): boolean {
+
+    const cookieExists = this.cookieService.check('token');
+
+    if (cookieExists) {
+      return true;
+    } else {
+      return false;
+    }
+
+  }
+
+  logoutUser(): void {
+    const cookieExists = this.cookieService.check('token');
+
+    if (cookieExists) {
+      this.cookieService.delete('token');
+      this.router.navigateByUrl('/auth/signin');
+    }
   }
 
 }
