09d70f7d7bcc38bbfea8e12ac2243e31a9370407
M Tayyab Younas <tayyabjagna@gmail.com>
2021-07-29 00:02:02 +0500

    profile forms errors fixed

--- src/app/modules/dashboard/informationform/informationform.component.ts
+++ src/app/modules/dashboard/informationform/informationform.component.ts
@@ -1,7 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { STEPPER_GLOBAL_OPTIONS } from '@angular/cdk/stepper';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { ProfileService } from '../profile/profile.service';
+import { MatStepper } from '@angular/material/stepper';
+import { PersonalInfo } from '../profile/models/Models';
+import { MatSnackBar } from '@angular/material/snack-bar';
 @Component({
   selector: 'app-informationform',
   templateUrl: './informationform.component.html',
@@ -15,17 +18,19 @@ import { ProfileService } from '../profile/profile.service';
 })
 export class InformationFormComponent implements OnInit {
 
+  @ViewChild('stepper') private stepper: MatStepper;
+
   personalInfoForm = new FormGroup({
     name: new FormControl(this.profileService.personalInfo.name || '', [Validators.required]),
     fatherName: new FormControl(this.profileService.personalInfo.fatherName || '', [Validators.required]),
     motherName: new FormControl(this.profileService.personalInfo.motherName || '', [Validators.required]),
     cnic: new FormControl(this.profileService.personalInfo.cnic || '', [Validators.required]),
     dob: new FormControl(this.profileService.personalInfo.dob || new Date(), [Validators.required]),
-    gender: new FormControl(this.profileService.personalInfo.gender || '', [Validators.required]),
+    gender: new FormControl(this.profileService.personalInfo.gender || 'Male', [Validators.required]),
     religion: new FormControl(this.profileService.personalInfo.religion || '', [Validators.required]),
-    bloodGroup: new FormControl(this.profileService.personalInfo.bloodGroup || '', [Validators.required]),
+    bloodGroup: new FormControl(this.profileService.personalInfo.bloodGroup || 'B+', [Validators.required]),
     passportNumber: new FormControl(this.profileService.personalInfo.passportNumber || '', [Validators.required]),
-    maritalStatus: new FormControl(this.profileService.personalInfo.maritalStatus || '', [Validators.required]),
+    maritalStatus: new FormControl(this.profileService.personalInfo.maritalStatus || 'Single', [Validators.required]),
     nameOfNextOfKin: new FormControl(this.profileService.personalInfo.nameOfNextOfKin || '', [Validators.required]),
     nextOfKinRelation: new FormControl(this.profileService.personalInfo.nextOfKinRelation || '', [Validators.required]),
   });
@@ -44,19 +49,65 @@ export class InformationFormComponent implements OnInit {
     username: new FormControl('', [Validators.required]),
   });
 
-  constructor(private profileService: ProfileService) { }
+  constructor(
+    private profileService: ProfileService,
+    private snackBar: MatSnackBar
+  ) { }
 
   ngOnInit(): void { }
 
-  stepperNext(stepper): any {
-    stepper.next();
+  stepperNext(): any {
+    this.stepper.next();
   }
 
-  stepperPrevious(stepper): any {
-    stepper.previous();
+  stepperPrevious(): any {
+    this.stepper.previous();
   }
 
-  submitPersonalInfo(): void { }
+  submitPersonalInfo(): void {
+    if (this.personalInfoForm.invalid) {
+      this.snackBar.open('Please fill all required fields', 'Ok', {
+        duration: 5000
+      });
+    }
+    const personalInfo = this.personalInfoForm.getRawValue();
+    this.profileService.personalInfo = new PersonalInfo(personalInfo);
+    this.stepperNext();
+    this.personalInfoForm.reset();
+  }
+
+  fromGuardianToPersonal(): void {
+    this.updatePersonalInfoFormValues();
+    this.stepperPrevious();
+  }
+
+  fromPersonalToGuardian(): void {
+    const personalInfo = this.personalInfoForm.getRawValue();
+    this.profileService.personalInfo = new PersonalInfo(personalInfo);
+    console.log(this.profileService.personalInfo);
+    this.stepperNext();
+    this.personalInfoForm.reset();
+  }
+
+  updatePersonalInfoFormValues(): void {
+
+    this.personalInfoForm.patchValue({
+
+      name: this.profileService.personalInfo.name,
+      fatherName: this.profileService.personalInfo.fatherName,
+      motherName: this.profileService.personalInfo.motherName,
+      cnic: this.profileService.personalInfo.cnic,
+      dob: this.profileService.personalInfo.dob,
+      gender: this.profileService.personalInfo.gender,
+      religion: this.profileService.personalInfo.religion,
+      bloodGroup: this.profileService.personalInfo.bloodGroup,
+      passportNumber: this.profileService.personalInfo.passportNumber,
+      maritalStatus: this.profileService.personalInfo.maritalStatus,
+      nameOfNextOfKin: this.profileService.personalInfo.nameOfNextOfKin,
+      nextOfKinRelation: this.profileService.personalInfo.nextOfKinRelation
+    });
+
+  }
 
   submitGardianInfo(): void { }
 
